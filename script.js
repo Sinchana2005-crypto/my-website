8aae3dac4c6b0810664662880277464842931252
Sinchana2005-crypto <sinchanashettyvb@gmail.com>
2025-11-08 13:01:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
+    // Fade in the title
     $("h1").hide().fadeIn(1000);
+
+    // Add color change on hover
+    $("h1").hover(function(){
+        $(this).css("color", "#e74c3c");
+    }, function(){
+        $(this).css("color", "white");
+    });
 });
+
